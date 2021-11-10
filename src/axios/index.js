534d3ee72557b20b91c68bc4813cd4f2b86fa70e
htet aung ko <htetaungko@yahoo.com>
2021-11-10 18:22:50 +0630

    fixed wrong env variable

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const API_URL = (import.meta.env.VUE_APP_ENV == 'production') ?
-    import.meta.env.VUE_APP_API_URL :
+const API_URL = (import.meta.env.VITE_APP_ENV == 'production') ?
+    import.meta.env.VITE_APP_API_URL :
     'http://localhost:8000'
 
 export default axios.create({
