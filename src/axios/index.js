4b85a08ae7bb725aecaf677818a7c0777b6ac9e2
htet aung ko <htetaungko@yahoo.com>
2021-11-10 18:21:08 +0630

    added app stage check point

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
+const API_URL = (import.meta.env.VUE_APP_ENV == 'production') ?
+    import.meta.env.VUE_APP_API_URL :
+    'http://localhost:8000'
+
 export default axios.create({
-    baseURL: 'http://localhost:8000'
-    
+    baseURL: API_URL
 })
